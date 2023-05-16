3725304eeec3d69d8c628caf343cf7dc5a892b6e
Tharindu <tharindujayasanka.official@gmail.com>
2023-05-17 00:04:39 +0530

    Sign up

--- GoFitness/Controllers/Other/SignUpViewController.swift
+++ GoFitness/Controllers/Other/SignUpViewController.swift
@@ -6,11 +6,149 @@
 //
 
 import UIKit
+import SnapKit
 
 class SignUpViewController: UIViewController {
-
+    
+    let imageView: UIImageView = {
+        let imageView = UIImageView()
+        imageView.image = UIImage(named: "signup-bg")
+        imageView.contentMode = .scaleAspectFill
+        imageView.clipsToBounds = true
+        return imageView
+    }()
+    
+    let subtitleLabel: UILabel = {
+        let label = UILabel()
+        label.text = "Welcome to GoFitness"
+        label.font = UIFont(name: "IntegralCF-Regular", size: 20)
+        label.textColor = UIColor(named: "primary")
+        label.textAlignment = .left
+        return label
+    }()
+    
+    let titleLabel: UILabel = {
+        let label = UILabel()
+        label.text = "Sign Up"
+        label.font = UIFont(name: "IntegralCF-Bold", size: 60)
+        label.textColor = .white
+        label.textAlignment = .left
+        return label
+    }()
+    
+    let emailTextField: UITextField = {
+        let textField = UITextField()
+        textField.placeholder = "Email"
+        textField.font = UIFont(name: "OpenSans-Regular", size: 17)
+        textField.textColor = .white
+        textField.addBottomBorder(color: UIColor(named: "bg-secondary") ?? .darkGray, height: 1)
+        return textField
+    }()
+    
+    let passwordTextField: UITextField = {
+        let textField = UITextField()
+        textField.placeholder = "Password"
+        textField.font = UIFont(name: "OpenSans-Regular", size: 17)
+        textField.textColor = .white
+        textField.isSecureTextEntry = true
+        textField.addBottomBorder(color: UIColor(named: "bg-secondary") ?? .darkGray, height: 1)
+        return textField
+    }()
+    
+    let rePasswordTextField: UITextField = {
+        let textField = UITextField()
+        textField.placeholder = "Retype Password"
+        textField.font = UIFont(name: "OpenSans-Regular", size: 17)
+        textField.textColor = .white
+        textField.isSecureTextEntry = true
+        textField.addBottomBorder(color: UIColor(named: "bg-secondary") ?? .darkGray, height: 1)
+        return textField
+    }()
+    
+    let signUpButton: UIButton = {
+        let button = UIButton(type: .system)
+        button.setTitle("Sign Up", for: .normal)
+        button.titleLabel?.font = UIFont(name: "OpenSans-SemiBold", size: 17)
+        button.setTitleColor(UIColor(named: "background"), for: .normal)
+        button.backgroundColor = UIColor(named: "primary")
+        button.layer.cornerRadius = 15
+        button.layer.cornerCurve = .continuous
+        return button
+    }()
+    
+    let googleSignUpButton: UIButton = {
+        let button = UIButton(type: .system)
+        button.setTitle("Sign Up with Google", for: .normal)
+        let googleImage = UIImage(named: "google")?.withRenderingMode(.alwaysOriginal)
+        button.setImage(googleImage, for: .normal)
+        button.titleLabel?.font = UIFont(name: "OpenSans-SemiBold", size: 17)
+        button.setTitleColor(.black, for: .normal)
+        button.backgroundColor = .white
+        button.layer.cornerRadius = 15
+        button.layer.cornerCurve = .continuous
+        
+        return button
+    }()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        view.backgroundColor = UIColor(named: "background")
+        view.addSubview(imageView)
+        view.addSubview(subtitleLabel)
+        view.addSubview(titleLabel)
+        view.addSubview(emailTextField)
+        view.addSubview(passwordTextField)
+        view.addSubview(rePasswordTextField)
+        view.addSubview(signUpButton)
+        view.addSubview(googleSignUpButton)
+
         
+        imageView.snp.makeConstraints { make in
+            make.top.leading.trailing.equalToSuperview()
+            make.width.equalToSuperview()
+        }
+        
+        subtitleLabel.snp.makeConstraints { make in
+            make.top.equalToSuperview().offset(200)
+            make.leading.trailing.equalToSuperview().inset(15)
+        }
+        
+        titleLabel.snp.makeConstraints { make in
+            make.top.equalTo(subtitleLabel.snp.bottom)
+            make.leading.trailing.equalToSuperview().inset(20)
+        }
+        
+        emailTextField.snp.makeConstraints { make in
+            make.top.equalTo(imageView.snp.bottom).offset(80)
+            make.leading.equalToSuperview().offset(50)
+            make.trailing.equalToSuperview().offset(-50)
+            make.height.equalTo(50)
+        }
+        
+        passwordTextField.snp.makeConstraints { make in
+            make.top.equalTo(emailTextField.snp.bottom).offset(20)
+            make.leading.trailing.equalTo(emailTextField)
+            make.height.equalTo(50)
+        }
+        
+        rePasswordTextField.snp.makeConstraints { make in
+            make.top.equalTo(passwordTextField.snp.bottom).offset(20)
+            make.leading.trailing.equalTo(emailTextField)
+            make.height.equalTo(50)
+        }
+        
+        signUpButton.snp.makeConstraints { make in
+            make.top.equalTo(rePasswordTextField.snp.bottom).offset(50)
+            make.centerX.equalToSuperview()
+            make.leading.trailing.equalTo(rePasswordTextField)
+            make.height.equalTo(55)
+        }
+        
+        googleSignUpButton.snp.makeConstraints { make in
+            make.top.equalTo(signUpButton.snp.bottom).offset(10)
+            make.centerX.equalToSuperview()
+            make.leading.trailing.equalTo(signUpButton)
+            make.height.equalTo(55)
+        }
     }
 }
